5027db95b74173c191a01d5df2713f346587a30a
nesin <nesin@192.168.0.157>
2022-04-26 21:19:26 +1000

    Target Bot Added functions for plot UR3 and for checking singularity

--- @TargetBot/TargetBot.m
+++ @TargetBot/TargetBot.m
@@ -11,18 +11,43 @@ classdef TargetBot < UR3
         % Target Parameters
         targetSize = 0.1; %Placeholder (metres)
 
+        % Target Hit, if hit == 1, the target has been hit and will move to
+        % new location
+
+        hit = 0;
     end
 
     methods
         function self = TargetBot()
             %TargetBot - TargetBot constructor, inheriting from UR3 baseclass
             %   Constructor calls base class
+
             self@UR3();
         end
 
-        function PlotTargetBot(self)
+        function PlotTargetBot(self,pos)
             %PlotTargetBot - TargetBot Plotting for simulation
             % Need to overwrite UR3 model plot to include target/camera in model
+            if  hit == 1
+            newq = self.ur3.model.ikine(pos);
+            qMatrix = jtraj(self.ur3.model.getpos(),newq,self.steps);
+            for i = 1:self.steps
+                self.ur3.model.animate(qMatrix(i,:));
+            end
+            hit = 0;
+            end
+        
+       
+        end
+
+        function NearSingularityM = CheckSingularity(self,q)
+            J = self.ur3.model.jacob(q);
+            m = sqrt(det(J*J))
+            if(m > 0.1)
+                NearSingularityM = 1;
+            else
+                NearSingularityM = 0;
+            end
         end
     end
 end
