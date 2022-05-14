d1d0756405cb2627f9e1c0c8dcf8f206c9d8356b
cptsausage <georgeparker369@hotmail.com>
2022-05-14 14:04:21 +1000

    Adding Target file for feature extraction and LaserBot stand alone code

---
+++ LaserBotStandAlone.m
@@ -0,0 +1,18 @@
+%% LaserBot Standalone Code
+close all;
+clear;
+
+% Initialise LaserBot
+laserBot = LaserBot();
+
+% Initialise Camera
+laserBot.CameraInit();
+laserBot.SetCamera(); % To set cameraTargets for error calculation
+
+% Initialise ROS connection
+rosshutdown;
+rosinit('192.168.0.253'); % UTS Pi address
+laserBot.ROSInit();
+
+laserBot.FindTarget();
+
