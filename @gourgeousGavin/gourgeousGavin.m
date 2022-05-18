8a28c5e4d99ecd443f8f90f114c8af77fe95728b
NickFromUTS <Nicholas.D.Jackson@student.uts.edu.au>
2022-05-18 10:56:27 +1000

    attempting to make a better light curtain test

---
+++ @gourgeousGavin/gourgeousGavin.m
@@ -0,0 +1,17 @@
+classdef gourgeousGavin
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+
+    properties
+        gavinModel;
+    end
+
+    methods
+        function obj = gourgeousGavin()
+            %UNTITLED2 Construct an instance of this class
+            %   Detailed explanation goes here
+            
+        end
+
+    end
+end
